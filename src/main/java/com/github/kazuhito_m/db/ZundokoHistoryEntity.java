499d3e105f67eedfe86a2f84fa715dda3a52f05b
kazuhito-m <kazuhito.sumpic@gmail.com>
2016-06-13 03:54:48 +0900

    DBに保存する機構を完成。FlywayでDBを作成しようとしたが…うまく同じDBと思ってくれず、課題が残る。

--- src/main/java/com/github/kazuhito_m/db/ZundokoHistoryEntity.java
+++ src/main/java/com/github/kazuhito_m/db/ZundokoHistoryEntity.java
@@ -2,20 +2,23 @@ package com.github.kazuhito_m.db;
 
 import lombok.Data;
 
-
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 
 @Entity
+@Table(name = "ZUNDOKO_HISTORY")
 @Data
 public class ZundokoHistoryEntity {
 
 
     @Id
+    @Column(name = "CLIENT_KEY")
     private String clientKey;
 
+    @Column(name = "HISTORY")
     private String history;
-
 
 }
