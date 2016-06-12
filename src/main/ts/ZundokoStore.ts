b37e79be17764cee44c71e7df5045e258e8911a0
kazuhito-m <kazuhito.sumpic@gmail.com>
2016-06-13 01:37:34 +0900

    コードアップ、とりあえずはサーバ側への送信だけ作成

--- src/main/ts/ZundokoStore.ts
+++ src/main/ts/ZundokoStore.ts
@@ -5,33 +5,78 @@ import ZundokoRecord from './ZundokoRecord';
 // 履歴情報を永続化するクラス。
 export default class ZundokoStore {
     // localStrageに保存するキー文字列。
-    private static KEY: string = 'zundokoButton';
+    private static KEY: string = 'zundokoSaveKey';
 
     // 内部保存するかフラグ
     public localSave: boolean = true;
 
     public load(): ZundokoRecord[] {
         let loaded: ZundokoRecord[] = [];
-        let json: string = localStorage.getItem(ZundokoStore.KEY);
-        if (this.localSave && json !== null) {
-            loaded = JSON.parse(json);
+
+        // まずはLocalStrageからキーを取得
+        let key: string = localStorage.getItem(ZundokoStore.KEY);
+        if (this.localSave && key !== null) {
+            // あるようなら、サーバに「このキーでデータくれ」と投げる。
+            $.get("loadJson",{ key: key } , (json) => {
+                // サーバから値がおとれるようなら
+                if (key !== null) {
+                    loaded = JSON.parse(json);
+                }
+            });
+
         }
+
         return loaded;
     }
 
     public save(nowRecord: ZundokoRecord, history: ZundokoRecord[]) {
         if (this.localSave) {
+
+
+            // まずはLocalStrageからキーを取得
+            let key: string = localStorage.getItem(ZundokoStore.KEY);
+            if (key !== null) {
+                // 無いなら、新しく「この端末専用」のキーを作成
+                key = this.makeId();
+                localStorage.setItem(ZundokoStore.KEY, key);
+            }
+
+
             let forSave: ZundokoRecord[] = [];
             // 一度でもボタンが押されてたら、
-            // 現在表示中のものまでを含んだ配列にし、JSONでローカル保存。
             if (nowRecord.line.length > 0) {
                 forSave = history.slice(0); // 手っ取り早い配列のコピー。
                 forSave.unshift(nowRecord);
             }
             // JSON文字列にしてlocalStrage保存。
             let json: string = JSON.stringify(forSave);
-            localStorage.setItem(ZundokoStore.KEY, json);
+            console.log(json);
+
+            // サーバに「このキーで保存して」を依頼
+             $.get("saveJson",{ key: key , json : json });
+
         }
     }
+
+    // UUID(っぽいもの)を作る。
+    private makeId(): string {
+        let pattern: number[] = [2, 1, 1, 1, 3];
+        let id: string = "";
+        for (let i = 0; i < pattern.length; i++) {
+            if (i > 0) {
+                id += "-";
+            }
+            for (let j = 0; j < pattern[i]; j++) {
+                id += this.makeRandStr4();
+            }
+        }
+        return id;
+    }
+
+    // ランダムな英数４字を作る。
+    private makeRandStr4(): string {
+        return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
+    }
+
 
 }
