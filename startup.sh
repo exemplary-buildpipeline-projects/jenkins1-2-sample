6bd8422cca3388cc4dcb89f092ba75dc2a25bd52
kazuhito-m <kazuhito.sumpic@gmail.com>
2016-11-03 14:35:16 +0900

    起動スクリプト変更。ReadMeに書くようなことを(おそらく俺が見返す時はスクリプト見るので)startup.shに足す。

--- startup.sh
+++ startup.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
+
+# ビルドもするなら、以下
+# mvn clean jar:jar
+
+# サーバ内でのアプリ起動。
 nohup java -jar ./*.jar > ./app.log &
+
+
+# 別の話として「デバッグする」なら、mvnのSpring Boot立ち上げコマンドで。
+mvn clean test spring-boot:run
+
